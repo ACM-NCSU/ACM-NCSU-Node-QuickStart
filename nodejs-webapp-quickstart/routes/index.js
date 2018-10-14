436c51eab19c2b31496ad3388391c900ba6993f8
Wade Moore <wpmoore2@ncsu.edu>
2018-10-14 14:24:53 -0400

    Add stuff

---
+++ nodejs-webapp-quickstart/routes/index.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Home Page' });
+});
+
+/* GET new page. */
+router.get('/login', function (req, res) {
+    res.render('login', { title: 'Login Page' });
+});
+
+
+module.exports = router;
