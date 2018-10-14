825fa0c47a4fab7d9e80ed6f4d4087864026b650
mattgd <hello@mattdzwonczyk.com>
2018-10-14 16:55:09 -0400

    Add basic order form and submit page.

--- nodejs-webapp-quickstart/routes/index.js
+++ nodejs-webapp-quickstart/routes/index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var router = express.Router();
 
+let orders = []; // "In-memory database" for orders
+
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Home Page' });
@@ -10,6 +12,20 @@ router.get('/', function (req, res) {
 /* GET new page. */
 router.get('/login', function (req, res) {
     res.render('login', { title: 'Login Page' });
+});
+
+/* POST a new order. */
+router.post('/order', function (req, res) {
+    let order = req.body;
+    order.position = orders.length + 1;
+    orders.push(order);
+
+    res.render('order_submitted', { title: 'Order Submitted', order });
+});
+
+/* GET order page. */
+router.get('/order', function (req, res) {
+    res.render('order', { title: 'Order Page' });
 });
 
 
