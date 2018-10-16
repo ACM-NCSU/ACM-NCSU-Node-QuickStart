0a9c329bfb745951c6d8e440e03259b47cd4955d
mattgd <hello@mattdzwonczyk.com>
2018-10-16 17:19:01 -0400

    Updates for workshop content.

--- nodejs-webapp-quickstart/routes/index.js
+++ nodejs-webapp-quickstart/routes/index.js
@@ -7,10 +7,4 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Home Page' });
 });
 
-/* GET new page. */
-router.get('/login', function (req, res) {
-    res.render('login', { title: 'Login Page' });
-});
-
-
 module.exports = router;
